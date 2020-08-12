1100d06de7008e99f35aa4ea309f631469929282
최진영 <Jinyoung.choi@hyundai-mnsoft.com>
2020-08-12 17:36:50 +0900

    2020/08/12 벽돌깨기 소스 업데이트

--- 2020_08_10/Bukdolggagi/Bukdolggagi/Source.cpp
+++ 2020_08_10/Bukdolggagi/Bukdolggagi/Source.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 #include <iostream>
 #include <queue>
+#define INF 987654321
+
+
 using namespace std;
 int map[100][100];
 int t,n,x,y;
@@ -9,12 +12,22 @@ int dx[] = { -1,1,0,0 };
 int dy[] = { 0,0,-1,1 };
 queue<pair<pair<int,int>,int>> q; 
 
+
+int N, W, H;
+//int map[16][12];
+
+//int dr[4] = { -1,1,0,0 };
+//int dc[4] = { 0,0,-1,1 };
+
+int ans = INF;
+
+
 void input()
 {
 	cin >> n >> x >> y ; 
-	for (int i = 0; i < x; i++)
+	for (int i = 1; i <=x; i++)
 	{
-		for (int j = 0; j < y; j++)
+		for (int j = 1; j <= y; j++)
 		{
 			int num;
 			cin >> num;
@@ -22,77 +35,122 @@ void input()
 		}
 	}
 } 
-void sol2(int sNum)
+// 정리해놓기 배열을 레퍼런스로 받는 방법
+
+void copyMap(int(* a)[100], int (* b)[100])
 {
-	for (int i = 0; i < 4; i++)
-	{ 
-		if (map[dx[i]][dy[i]] == 0 )
-			continue;
-		else if (map[dx[i]][dy[i]] > sNum)
+	for (int i = 0; i <= H; i++)
+	{
+		for (int j = 0; i < W; j++)
 		{
-		//	sol2(sNum);
+			b[i][j] = a[i][j];
 		}
-	 
 	}
 }
+
+void downMap()
+{
+	for (int i = 0; i < W; i++)
+	{
+		queue<int> down;
+		//차근차근 위에서부터 맨아래로
+		for (int r = H; r > 0; r--)
+		{
+			if (map[r][i] > 0)
+			{
+				down.push(map[r][i]);
+			}
+
+		}
+		//아래서부터 맨위까지 0으로 채움
+		for (int r = 1; r < H; r++)
+		{
+			map[r][i] = 0;
+		}
+		int h = H;
+		//위에서 부터 큐에서 빼면서 순서대로 쭉 채워잔다.
+		while (!down.empty())
+		{
+			int range = down.front();
+			down.pop();
+			map[h][i] = range;
+			h--;
+		}
+	} 
+}
+
+void drop(int r, int c, int range) 
+{
+	if (range == 0)
+	{
+		if (r + 1 > H)
+		{
+			return;
+		}
+		//만약 해당 값이 0이면 계속 탐색한다.
+		drop(r + 1, c, map[r + 1][c]);
+	}
+	else
+	{
+		//중요 queue
+		queue<pair<pair<int, int>, int>> q;
+		q.push({ { r,c }, range });
+		map[r][c] = 0;
+		while (!q.empty())
+		{
+			int rr = q.front().first.first;
+			int cc = q.front().first.second;
+			int nRange = q.front().second;
+			q.pop();
+			for (int i = 1; i < nRange; i++)
+			{
+				for (int j = 1; j < 4; j++)
+				{
+					int nx = rr + i*dx[j];
+					int ny = cc + i*dy[j];
+					if (nx<1 || nx>H || ny<0 || ny>W - 1)
+					{
+						continue;
+					}
+					if (map[nx][ny] > 0)
+					{
+						q.push({ {nx,ny},map[nx][ny] });
+						map[nx][ny] = 0;
+					}
+				}
+			} 
+		}
+
+		downMap();
+	}
+}
+
+ 
 void sol(int fNum)
 {
-    //bombs.push({ { x좌표,y좌표 },해당 값 });
-	//q.push( { { x , y } , value } );
-	//가로 탐색
-	for (int i = 0; i < x; i++) 
+	if (fNum == 0)
 	{
-		if (map[i][0] == 0)
+		int sum = 0;
+		for (int i = 1; i <= H; i++)
 		{
-			continue;
-		}
-		else
-		{
-			//해당 값
-			//int tempNum = map[i][fNum];
-			//q.push( tempNum , { i,0 });
-			q.push({ { i,fNum }, map[i][fNum] });
-			while (!q.empty())
+			for (int j = 1; j <= W; j++)
 			{
-				int value = q.front().second; // 3,0 
-				q.pop();
-				//가로줄
-				for (int w = 0; w < value; w++)
+				if (map[i][j] > 0)
 				{
-					if (w == i)
-						continue;
-					if (map[w][fNum] > 1)
-					{
-						q.push({ { w,i }, map[w][fNum] });
-						map[w][fNum] = 0;
-					}
-					else if (map[w][fNum] == 0)
-						continue;
-					else
-						map[w][fNum] = map[w][fNum] - value;
+					sum++;
 				}
-				//세로줄
-
-				for (int w = 0; w < value; w++)
-				{
-
-					if (w == i)
-						continue;
-					if (map[i][w] > 1)
-					{
-						q.push({ { w,i }, map[w][fNum] });
-						map[i][w] = 0;
-					}
-					else if (map[i][w] == 0)
-						continue;
-					else
-						map[i][w] = map[i][w] - value;
-				}
-
-			 }
-
+			}
+			if (ans > sum)ans = sum;
+			return;
 		}
 	}
+	int copyM[100][100];
+	for (int i = 0; i < W; i++)
+	{
+		copyMap(map, copyM);
+
+	}
+ 
 }
 int main()
 {
--- 2020_08_10/Bukdolggagi/Bukdolggagi/Source1.cpp
+++ 2020_08_10/Bukdolggagi/Bukdolggagi/Source1.cpp
@@ -13,9 +13,13 @@ int dc[4] = { 0,0,-1,1 };
 
 int ans = INF;
 
-void copyMap(int(*a)[12], int(*b)[12]) {
-	for (int i = 1; i <= H; i++) {
-		for (int j = 0; j < W; j++) {
+// 초기맵을 카피해야함.
+void copyMap(int(*a)[12], int(*b)[12]) 
+{
+	for (int i = 1; i <= H; i++)
+	{
+		for (int j = 0; j < W; j++)
+		{
 			b[i][j] = a[i][j];
 		}
 	}
@@ -47,33 +51,42 @@ void blockSort() {
 
 void dropTheBall(int r, int c, int range) 
 {
+	//range==0 이라는 것은 map[x][y] == 0 이라는 것 
 	if (range == 0) 
 	{
 		if (r + 1 > H) return;
-		dropTheBall(r + 1, c, map[r + 1][c]);
+		dropTheBall(r + 1, c, map[r + 1][c]); //r+1를 해주면서 (= 아래로 내리면서) 탐색
 	}
 	else {
 		queue<pair<pair<int, int>, int>> bombs;
-		bombs.push({ { r,c },range });
-		map[r][c] = 0;
+		bombs.push({ { r,c },range });  //좌표와 그 값(range)를 저장
+		map[r][c] = 0;  //해당 좌표를 0 으로 초기화
+
 		while (!bombs.empty()) 
 		{
-			int curR = bombs.front().first.first;
-			int curC = bombs.front().first.second;
-			int curRange = bombs.front().second;
-			bombs.pop();
-			map[curR][curC] = 0;
+			int curR = bombs.front().first.first;  //현재 R
+			int curC = bombs.front().first.second; //현재 C 
+			int curRange = bombs.front().second; //범위
+			bombs.pop();  
+			map[curR][curC] = 0;  // 엥 왜 두번하지?
+			// 범위 만큼 
 			for (int i = 1; i < curRange; i++) 
-			{
+			{ 
+				//상하좌우라서 4번
 				for (int j = 0; j < 4; j++) 
 				{
+					//상하좌우 0*dr[0] , 1*dr[1]  
 					int nr = curR + i*dr[j];
 					int nc = curC + i*dc[j];
+					//범위 밖
 					if (nr < 1 || nr > H || nc < 0 || nc > W - 1)
 						continue;
+					//만약 그자리가 0보다 크다면
 					if (map[nr][nc] > 0) 
 					{
-						bombs.push({ { nr,nc },map[nr][nc] });
+						//또 터트려야하니깐 queue에 넣기.
+						bombs.push({ { nr,nc },map[nr][c] });
+						//해당 자리0으로 초기화 어차피 0으로 변함.
 						map[nr][nc] = 0;
 					}
 				}
@@ -85,6 +98,7 @@ void dropTheBall(int r, int c, int range)
 
 void playball(int n) 
 {
+	//n==0 이라는것은 n번의 기회를 다 쓴것 
 	if (n == 0) 
 	{
 		int sum = 0;
@@ -99,11 +113,19 @@ void playball(int n)
 		return;
 	}
 	int copyM[16][12];
+	//10번 실행
 	for (int i = 0; i < W; i++) 
 	{
+		/*
+			총 W=10일때 10번 수행한다.
+			기존 원본 맵을 카피해준다.
+			1) dropthball 0이 아닌 숫자를 찾으면 그로부터 터지는 경우를 모두 구한다. queue이용
+			2) blockSort() 값이 터지면 모든 값을 queue에 넣고 아래서 부터 순서대로 삽입한다. 13''2 --> 132 이런식. queue 이용
+			3) playball (n-1) 0보다 큰 숫자의 남아있는 공을 모두 구한다. 그 값이 ans 보다 작으면 ans에 저장하여 min값을 구한다.
+		*/
 		copyMap(map, copyM);
-		dropTheBall(0, i, 0);
-		playball(n - 1);
+		dropTheBall(0, i, 0); 
+		playball(n - 1);//n-1해주는것은 한번씩 기회를 날리는 것
 		copyMap(copyM, map);
 	}
 }
