6adda8b6095b7e5762a75a7e17b2002da2d46189
최진영 <Jinyoung.choi@hyundai-mnsoft.com>
2020-10-30 16:35:14 +0900

    2020/10/29 늑대문제, 안전영역

---
+++ 20_하반기코테대비/20_하반기코테대비/백준_nnnn_늑대와양.cpp
@@ -0,0 +1,88 @@
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <functional>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+
+/*
+6 6
+..S...
+..S.W.
+.S....
+..W...
+...W..
+......
+
+
+.DSD..
+.DSDW.
+DSD...
+.DW...
+...W..
+......
+
+*/
+int n, m;
+string map[101][101] = { "", };
+int dx[] = { -1,1,0,0 };
+int dy[] = { 0,0,-1,1 };
+bool check = false;
+int main()
+{
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		string temp;
+		cin >> temp;
+		for (int j = 0; j < m; j++)
+		{
+			map[i][j] = temp[j];
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (map[i][j] == "S")
+			{
+				for (int w = 0; w < 4; w++)
+				{
+					int nx = i + dx[w];
+					int ny = j + dy[w];
+					if (nx<0 || nx>n || ny > m || ny < 0)
+						continue;
+					if (map[nx][ny] == "W")
+					{
+						check = true;
+						cout << "0" << endl;
+						exit(0);
+					}
+					if (map[nx][ny] == "D")
+						continue;
+					if (map[nx][ny] == ".")
+						map[nx][ny] = "D";
+				}
+			}
+		}
+	}
+	if (check)
+		cout << 0 << endl;
+	else
+	{
+		cout << "1" << endl;
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				cout << map[i][j];
+			}
+			cout << endl;
+		}
+
+	}
+	return 0;
+}
---
+++ 20_하반기코테대비/20_하반기코테대비/백준_nnnn_안전영역.cpp
@@ -0,0 +1,104 @@
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <functional>
+#include <vector>
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+/*
+5
+6 8 2 6 2
+3 2 3 4 6
+6 7 3 3 2
+7 2 5 3 6
+8 9 5 2 7
+
+
+*/
+int n;
+int map[101][101];
+int dx[] = { -1,1,0,0 };
+int dy[] = { 0,0,-1,1 };
+int maxNum = -987654321;
+bool visit[101][101] = { false, };
+int cnt = 0;
+int maxmax = 0;
+void jangma(int w)
+{
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (map[i][j] <= w)
+				map[i][j] = -1;
+		}
+	}
+
+}
+void bfs(int x, int y)
+{
+	queue<pair<int, int>> q;
+	q.push({ x,y });
+	visit[x][y] = true;
+	while (!q.empty())
+	{
+		int xx = q.front().first;
+		int yy = q.front().second;
+		q.pop();
+		for (int w = 0; w < 4; w++)
+		{
+			int nx = xx + dx[w];
+			int ny = yy + dy[w];
+
+			if (nx<0 || nx >= n || ny<0 || ny >= n)
+				continue;
+			if (visit[nx][ny])
+				continue;
+			if (map[nx][ny] == -1)
+				continue;
+			if (map[nx][ny] != -1)
+				visit[nx][ny] = true;
+			q.push({ nx,ny });
+		}
+	}
+	cnt++;
+
+}
+int main()
+{
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+
+			cin >> map[i][j];
+			if (maxNum < map[i][j])
+				maxNum = map[i][j];
+		}
+	}
+	for (int w = 2; w < maxNum; w++)
+	{
+
+		cnt = 0;
+		jangma(w);
+		memset(visit, false, sizeof(visit));
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (map[i][j] != -1 && visit[i][j] == false)
+					bfs(i, j);
+				if (cnt > maxmax)
+					maxmax = cnt;
+			}
+		}
+	}
+	cout << maxmax << endl;
+	return 0;
+}
